9e4a94818dca12fc2bcb73f19f485560860513c8
Keith Sharp <kms@passback.co.uk>
2021-07-07 12:58:13 +0100

    MetalRendererTest basic setup.

---
+++ MetalRendererTests/MetalRendererTests/MetalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MetalViewController.swift
+//  MetalRendererTests
+//
+//  Created by Keith Sharp on 07/07/2021.
+//
+
+import Cocoa
+
+class MetalViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ MetalRendererTests/MetalRendererTests/OptionsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OptionsViewController.swift
+//  MetalRendererTests
+//
+//  Created by Keith Sharp on 07/07/2021.
+//
+
+import Cocoa
+
+class OptionsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
