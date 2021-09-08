f0d218fb14d91b9d7e603f4bff69d7d5c9e67492
Keith Sharp <kms@passback.co.uk>
2021-09-08 16:51:06 +0100

    Displaying different SwiftUI sheets based on an enum.

---
+++ MultiSheet/MultiSheet/MultiSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiSheetApp.swift
+//  MultiSheet
+//
+//  Created by Keith Sharp on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct MultiSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
